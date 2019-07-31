bb51c978256b7f022ad97adabd7493c042cc2f68
Robert Wikman <rbw@vault13.org>
2019-08-01 00:21:35 +0200

    Add default path
    Signed-off-by: Robert Wikman <rbw@vault13.org>

--- aioli_guestbook/config.py
+++ aioli_guestbook/config.py
@@ -2,4 +2,5 @@ from aioli.config import PackageConfigSchema, fields
 
 
 class ConfigSchema(PackageConfigSchema):
+    path = fields.String(missing="/guestbook")
     visits_max = fields.Integer(required=True)
